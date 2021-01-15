13898bd76006c69fab10d79bfc4390e82555738f
Bertrand Wilden <bert@bwilden.com>
2021-01-15 01:20:15 -0600

    added more on probability distributions

--- week-2/code/lab2.R
+++ week-2/code/lab2.R
@@ -1,7 +1,228 @@
 ###############################
 ##           Lab 2           ##
+##        Probability,       ##
 ## Simulations and Functions ##
 ###############################
+
+
+############################
+## Distribution Functions ##
+
+# R provides four functions for each common probability distribution
+# These begin with a prefix followed by an abbreviation for the distribution
+# (e.g. dnorm, pnorm, qnorm, rnorm for the Normal distribution)
+  # d* The density function. The height of the curve at a particular point
+  # p* The cumulative density function. The probability below some threshold
+  # q* The quantile function. This is the inverse of the CDF. 
+     # "Useful" for defining intervals containing a given probability density (e.g. 95%)
+  # r* Random sampling. Draw random samples from a particular distribution
+
+
+## Density ##
+# Density function for the Normal distribution: dnorm
+curve(dnorm, lwd = 2, xlim = c(-4,  4), ylab = "Density", main = "Normal PDF")
+
+# The height of the distribution at a particular value
+dnorm(x = 0)
+
+# Let's see what happens when we change the mean
+x <- seq(-4, 4, by = 0.01)
+plot(x = x, y = dnorm(x, mean = 1), type = "l", lwd = 2, ylab = "Density", main = "Normal PDF, mu = 2")
+# Do you expect x = 0 to be greater, smaller, or the same as before?
+dnorm(x = 0, mean = 1)
+
+# What about when we change the standard deviation?
+plot(x = x, y = dnorm(x, sd = 2), type = "l", lwd = 2, ylab = "Density", main = "Normal PDF, sigma = 2")
+dnorm(x = 0, sd = 2)
+# Why is x = 0 smaller when we increase the standard deviation but keep the mean constant?
+
+# We can do this with other distributions too!
+# Density function for the Binomial distribution: dbinom
+x <- seq(0, 5)
+plot(x = x, y = dbinom(x, size = 5, prob = 0.7), type = "h", lwd = 4, ylab = "Density", main = "Binomial PMF \n size = 5, prob = 0.7")
+# The height of the bar at x = 2
+dbinom(2, 5, 0.7)
+
+# If we change the probability parameter to 0.9, how will this change the height of x = 2?
+dbinom(2, 5, 0.9)
+
+### Exercise 1:
+# What is the probability of getting exactly one head if we flip a fair coin 7 times?
+
+
+# Each commonly-used distribution has its own version of these functions
+dpois(3, lambda = 2)  # The probability that a draw from X ~ Poisson(2) is exactly 3
+
+x <- seq(0, 20)
+plot(x, dpois(x, lambda = 5), type = "h", lwd = 2, ylab = "Density", main = "Poisson PMF \n lambda = 5")
+
+# Beta Distribution (useful for Bayesian statistics)
+curve(dbeta(x, shape1=.1, shape2=.1), lwd=2, xlim=c(0,  1), xlab="p", main="Beta(.1, .1)")
+curve(dbeta(x, shape1=1, shape2=1), lwd=2, xlim=c(0,  1), xlab="p", main="Beta(1, 1)")
+curve(dbeta(x, shape1=10, shape2=10), lwd=2, xlim=c(0,  1), xlab="p", main="Beta(10, 10)")
+curve(dbeta(x, shape1=1000000, shape2=1000000), lwd=2, xlim=c(0,  1), xlab="p", main="Beta(1,000,000, 1,000,000)")
+curve(dbeta(x, shape1=2, shape2=20), lwd=2, xlim=c(0,  1), xlab="p", main="Beta(2, 20)")
+curve(dbeta(x, shape1=1, shape2=2), lwd=2, xlim=c(0,  1), xlab="p", main="Beta(1, 2)")
+
+
+
+## Cumulative Density ##
+# Cumulative density function for the Normal distribution: pnorm
+curve(pnorm, lwd = 2, xlim = c(-3,  3), ylab = "Density", xlab = "X", main = "Normal CDF")
+
+# The CDF tells you the probability below (to the left of) some threshold on the PDF
+pnorm(0)
+# The probability below the -1.96 threshold
+pnorm(-1.96)
+# The probability between -1.96 and +1.96
+1 - 2 * pnorm(-1.96)
+
+# We can also use the mean and sd arguments to get other normal curves
+pnorm(2, mean = 3, sd = 1)          # Probability that x <= 2 if X ~ N(3, 1)
+1 - pnorm(2, 3, 1)                  # Probability that x > 2 if X ~ N(3, 1)
+pnorm(4, 4, 2) - pnorm(2, 4, 2)     # Probability that 2 < x <= 4 if X ~ N(4, 2^2)
+
+## Why does that work?
+
+# pnorm(2, 4, 2) gives us this:
+x <- seq(-4, 12, by = .01)
+plot(x, dnorm(x, 4, 2), type="l", lwd=2, xlim=c(-2, 10), main="P(x < 2)", ylab="Density")
+x1 <- seq(-8, 2, by = .01)
+polygon(c(x1, rev(x1)), c(dnorm(x1, 4, 2), rep(0, length(x1))), col="forestgreen")
+pnorm(2, 4, 2)
+
+# pnorm(4, 4, 2) gives us this:
+x <- seq(-4, 12, by=.01)
+plot(x, dnorm(x, 4, 2), type="l", lwd=4, xlim=c(-2, 10), main="P(x < 4)", ylab="Density")
+x1 <- seq(-8, 4, by=.01)
+x2 <- seq(-8, 2, by=.01)
+polygon(c(x1, rev(x1)), c(dnorm(x1, 4, 2), rep(0, length(x1))), col="blue")
+polygon(c(x2, rev(x2)), c(dnorm(x2, 4, 2), rep(0, length(x2))), col="forestgreen")
+pnorm(4, 4, 2)
+
+# pnorm(4, 4, 2) - pnorm(2, 4, 2) gives us the area between 2 and 4
+plot(x, dnorm(x, 4, 2), type="l", lwd=4, xlim=c(-2, 10), main="P(2 < x < 4)", ylab="Density")
+x1 <- seq(2, 4, by = .01)
+polygon(c(x1, rev(x1)), c(dnorm(x1, 4, 2), rep(0, length(x1))), col="blue")
+pnorm(4, 4, 2) - pnorm(2, 4, 2)
+
+
+### Exercise 2:
+# Assuming a standard normal distribution (X ~ N(0, 1)), calculate the 
+# probability that 0 < X < 1
+
+
+
+### Exercise 3: Connecting pbimon to dbinom
+# How would you use pbinom to calculate the probability of 2 heads in 3 flips, dbinom(2, 3, 0.5)
+dbinom(2, 3, 0.5)
+
+
+
+# Play around with distributions at: https://ben18785.shinyapps.io/distribution-zoo/
+# Also includes the R code for pdf/cdfs. And Latex equations!
+
+
+
+## Quantile Functions ##
+plot(qnorm, ylab = "X", xlab = "Probability")
+
+# What is the value for the 75th percentile of N(0, 1)
+qnorm(0.75)
+# 25th percentile? What do you notice?
+qnorm(0.25)
+
+# What is the 95% interval around the mean of N(20, 3^2)
+qnorm(0.05, 20, 3)
+qnorm(.95, 20, 3)
+
+
+
+## Sampling from Distributions ##
+# PDFs and CDFs are mathematical constructions, but they are NOT data!
+# They describe the process by which data could be generated
+# Often the best way to understand probability distributions is to general random samples or draws
+
+rnorm(1)                         # 1 draw from the distribution X ~ N(0, 1)
+rnorm(50, mean = 20, sd = 2)     # 50 draws from the distribution X ~ N(20, 2^2)
+
+# Lists of numbers are hard to look at! Let's graph instead
+hist(rnorm(10), col = "steelblue", xlab = "X")
+hist(rnorm(100), col = "steelblue", xlab = "X")
+hist(rnorm(1e7), col = "steelblue", xlab = "X")
+
+# Binomial time
+rbinom(100, 1, 0.5)      # 100 flips of 1 coin
+rbinom(100, 5, 0.5)      # 100 flips of 5 coins. Values are counts of "success"
+
+hist(rbinom(10, 1, 0.5), col = "steelblue", xlab = "X")
+hist(rbinom(10000, 1, 0.5), col = "steelblue", xlab = "X")
+hist(rbinom(10, 5, 0.7), col = "steelblue", xlab = "X")
+hist(rbinom(10000, 5, 0.7), col = "steelblue", xlab = "X")
+
+# Poisson poisson poisson
+rpois(1000, lambda = 4)          # 1000 draws from the poisson distribution with lambda=4
+
+hist(rpois(10000, lambda = 4), col = "steelblue", xlab = "X") 
+
+# Uniform is boring!
+# But sometimes useful :)
+runif(1000, min = -1, max = 1)   # 1000 draws from uniform distribution from -1 to +1
+
+hist(runif(10, min = -1, 1), col = "steelblue", xlab = "X", xlim = c(-3, 3))
+hist(runif(1000, min = -1, 1), col = "steelblue", xlab = "X", xlim = c(-3, 3))
+
+
+
+## Z Scores ##
+# Let's make an easy vector to work with
+x <- c(0, 1, 3, 5, 6)
+
+# Now we can "standardize" it by transforming into z-scores
+x_z <- (x - mean(x)) / sd(x)
+x_z
+
+hist(x, br = 3)
+hist(x_z, br = 3)
+
+# It can turn any distribution into a Normal(0, 1)?
+x_draws <- runif(1000, 0, 100)
+z <- (x_draws - mean(x_draws)) / sd(x_draws)
+
+mean(x_draws)
+mean(z)
+var(x_draws)
+var(z)
+hist(x_draws)
+hist(z)
+
+# What is the point of all this?
+# Frequently used in regression contexts. We will discuss this later
+# Z-tests
+
+# Starting 5 for the UCSD Tritons
+nba_heights <- c(190, 170, 206, 162, 180)
+
+# Average height and standard deviation of the UCSD student body
+avg_height <- 171
+sd_height <- 10
+
+# Creating a z statistic
+se <- sd_height/sqrt(length(nba_heights))
+z <- (mean(nba_heights) - avg_height) / se
+z
+
+# The p-value!
+2 * (1 - pnorm(z))
+
+curve(dnorm, lwd = 2, xlim = c(-6,  6), ylab = "Density", main = "Normal PDF")
+abline(v = z, lwd = 2, col = "red")
+
+
+## ---------------------------------------------------------------------- ##
+##                                  Break                                 ##
+## ---------------------------------------------------------------------- ##
+
 
 
 
@@ -38,7 +259,8 @@ num_vec[1] <- 7
 num_vec
 
 # Adding new elements to a vector
-longer_vec <- c(num_vec, 9)
+longer_vec[8] <- 10 
+longer_vec <- c(longer_vec, 9)
 longer_vec
 
 
@@ -56,10 +278,10 @@ vec_1 <- c(1, 2, 3)
 vec_2 <- c(4, 5, 6)
 vec_3 <- c(7, 8, 9)
 
-# rbind "row bind" vectors by stacking them on top of each other
+# rbind "row bind" vectors by stacking them on top of each other to form a matrix
 rbind(vec_1, vec_2, vec_3)
 
-# cbind "column bind" vectors by placing them side by side
+# cbind "column bind" vectors by placing them side by side to form a matrix
 cbind(vec_1, vec_2, vec_3)
 
 # rbind and cbind are VERY useful for adding additional columns or rows 
@@ -78,73 +300,6 @@ mat[1, ]   # First row
 # Arrays are higher dimension vectors. Rare in our cases!
 # Lists are like vectors whose elements can be any other type of object
 #     sometimes useful to know about
-
-
-
-###########################
-## Distribution Functions ##
-# Density function for the Normal distribution: dnorm
-curve(dnorm, lwd = 2, xlim = c(-3,  3), ylab = "Density", xlab = "X", main = "Normal PDF")
-
-# Cumulative density function for the Normal distribution: pnorm
-curve(pnorm, lwd = 2, xlim = c(-3,  3), ylab = "Density", xlab = "X", main = "Normal CDF")
-
-# Recall: the CDF tells you the probability below (to the left of) some threshold
-pnorm(0)
-# The probability below the -1.96 threshold
-pnorm(-1.96)
-# The probability between -1.96 and +1.96
-1 - 2*pnorm(-1.96)
-
-# We can also use the mean and sd arguments to get other normal curves
-pnorm(2, mean = 3, sd = 1)          # Probability that x < 2 if X ~ N(3, 1)
-1 - pnorm(2, 3, 1)                  # Probability that x > 2 if X ~ N(3, 1)
-pnorm(4, 4, 2) - pnorm(2, 4, 2)     # Probability that 2 < x < 4 if X ~ N(4, 2^2)
-
-## Why does that work?
-
-# pnorm(2, 4, 2) gives us this:
-x <- seq(-4, 12, by = .01)
-plot(x, dnorm(x, 4, 2), type="l", lwd=2, xlim=c(-2, 10), main="P(x < 2)", ylab="Density")
-x1 <- seq(-8, 2, by = .01)
-polygon(c(x1, rev(x1)), c(dnorm(x1, 4, 2), rep(0, length(x1))), col="forestgreen")
-pnorm(2, 4, 2)
-
-# pnorm(4, 4, 2) gives us this:
-x <- seq(-4, 12, by=.01)
-plot(x, dnorm(x, 4, 2), type="l", lwd=4, xlim=c(-2, 10), main="P(x < 4)", ylab="Density")
-x1 <- seq(-8, 4, by=.01)
-x2 <- seq(-8, 2, by=.01)
-polygon(c(x1, rev(x1)), c(dnorm(x1, 4, 2), rep(0, length(x1))), col="blue")
-polygon(c(x2, rev(x2)), c(dnorm(x2, 4, 2), rep(0, length(x2))), col="forestgreen")
-pnorm(4, 4, 2)
-
-# pnorm(4, 4, 2) - pnorm(2, 4, 2) gives us the area between 2 and 4
-plot(x, dnorm(x, 4, 2), type="l", lwd=4, xlim=c(-2, 10), main="P(2 < x < 4)", ylab="Density")
-x1 <- seq(2, 4, by=.01)
-polygon(c(x1, rev(x1)), c(dnorm(x1, 4, 2), rep(0, length(x1))), col="blue")
-pnorm(4, 4, 2) - pnorm(2, 4, 2)
-
-
-### Exercise 1:
-# Assuming a standard normal distribution (X ~ N(0, 1)), calculate the 
-# probability that 0 < X < 1
-
-
-
-# Each commonly-used distribution has its own version of these functions
-dpois(3, lambda = 2)  # The probability that a draw from X ~ Poisson(2) is exactly 3
-
-x <- seq(0, 20)
-plot(x, dpois(x, lambda = 5), lwd = 2, ylab = "Density", main = "Poisson PMF lambda = 5")
-
-# Beta Distribution (useful for Bayesian statistics)
-curve(dbeta(x, shape1=.1, shape2=.1), lwd=2, xlim=c(0,  1), xlab="p", main="Beta(.1, .1)")
-curve(dbeta(x, shape1=1, shape2=1), lwd=2, xlim=c(0,  1), xlab="p", main="Beta(1, 1)")
-curve(dbeta(x, shape1=10, shape2=10), lwd=2, xlim=c(0,  1), xlab="p", main="Beta(10, 10)")
-curve(dbeta(x, shape1=1000000, shape2=1000000), lwd=2, xlim=c(0,  1), xlab="p", main="Beta(1,000,000, 1,000,000)")
-curve(dbeta(x, shape1=2, shape2=20), lwd=2, xlim=c(0,  1), xlab="p", main="Beta(2, 20)")
-curve(dbeta(x, shape1=1, shape2=2), lwd=2, xlim=c(0,  1), xlab="p", main="Beta(1, 2)")
 
 
 
@@ -187,6 +342,14 @@ convert_temp(75, scale = "celsius")
 convert_temp(75) # Same thing as above because scale = "celsius" when we defined the function originally
 convert_temp(75, scale = "kelvin")
 convet_temp(75, scale = "fahrenheit") # error!
+
+# Z Score function
+convert_z <- function(x) {
+  z <- (x - mean(x)) / sd(x)
+  
+  return(z)
+}
+
 
 # Tips for functions!
    # Before starting, think clearly about what you want the output of the function to look like
@@ -289,21 +452,6 @@ best_pet <- sample(pets, size = 1)
 # R can reveal truths about the world...
 print(paste0("The best pet is ", best_pet))
 
-## Sampling from Distributions ##
-# The sample() function generates random draws from vectors/distributions that we define ourselves
-# What if we want to sample from known distributions, such as the Normal or Poisson?
-# R has pdf/pmf sampling functions that mirror the syntax of the distribution functions we used earlier (dnorm and pnorm)
-
-rnorm(n = 1)                     # 1 draw from the distribution X ~ N(0, 1)
-rnorm(50, mean = 20, sd = 2)     # 50 draws from the distribution X ~ N(20, 2^2)
-rpois(1000, lambda = 3)          # 1000 draws from the poisson distribution with lambda=3
-rbinom(1, size = 1, prob = 0.5)  # 1 draw from a binomial distribution with p=0.5 and 1 event (Bernoulli distribution)
-rbinom(100, 10, 0.75)            # 100 draws from binomial distribution with p=0.75 and 10 events
-
-# Visualizing these samples with histograms is an alternative to curve() with density functions
-hist(rnorm(10000))
-hist(rpois(10000, lambda=3))   
-hist(rbinom(10000, 5, 0.75))
 
 
 ###################
@@ -363,10 +511,10 @@ hist(results)
 # Note that this is asking for the standard deviation of a distribution of means!
 
 # First, let's see what 100 draws from the geometric distribution looks like
-hist(rgeom(100, prob=0.3))
+hist(rgeom(100, prob = 0.3))
 
 # We want to know something about how the *mean* of these draws is distributed
-mean(rgeom(100, prob=0.3))
+mean(rgeom(100, prob = 0.3))
 
 # Now let's do this 100,000 more times and save the results
 N <- 100000
@@ -376,8 +524,8 @@ number_draws <- 100
 # N is the number of iterations in the loop; 
 # number.draws is the number of draws from the geometric distribution 
 #              that we take in each iteration
-for(i in 1:N) {
-  draws <- rgeom(number_draws, prob=0.3)
+for (i in 1:N) {
+  draws <- rgeom(number_draws, prob = 0.3)
   means_outcomes[i] <- mean(draws)
 }
 sd(means_outcomes)
@@ -389,14 +537,16 @@ curve(dnorm(x, mean(means_outcomes), sd(means_outcomes)), add = T, lwd = 3)
 
 # The replicate() function is useful if we just want to run the same function over and over N times
 pets <- c("dog", "cat", "monkey", "parrot")
-generate_random_pet <- function() {  # Note: functions do not need any arguments!
+generate_random_pet <- function() {  # Note: functions do not necessarily need any arguments!
   rand_pet <- sample(pets, 1)
 }
 replicate(10, generate_random_pet())
 
 
 # Pancake problem!!!
-# simulate a pancake and return randomly ordered sides
+# What is the probability that the other side of a pancake is burnt
+# given that you are served a burnt side face up
+# Simulate a pancake and return randomly ordered sides
 sim_pancake <- function() {
   pancake <- sample(1:3, 1)
   sides <- matrix(c(1, 1, 1, 0, 0, 0), nrow = 2, ncol = 3)[, pancake] 
